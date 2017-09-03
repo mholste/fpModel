e498239daaffc0ddda9a33cc3c4915bdd5ae4d91
Micha <Micha@DESKTOP-61SU648.fritz.box>
2017-09-03 12:36:07 +0200

    weiter normalisiert: werte raus, in bank rein

--- src/de/mho/finpim/persistence/model/Account.java
+++ src/de/mho/finpim/persistence/model/Account.java
@@ -19,17 +19,12 @@ public class Account
 	@JoinColumn(name="pId")
 	protected Person person;
 	
+	@ManyToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="bId")
+	protected Bank bank;
+	
 	@Basic
 	protected int accNo;
-	
-	@Basic
-	protected String access;
-	
-	@Basic
-	protected String PIN;
-	
-	@Basic
-	protected int blz;
 	
 	protected long balance;
 
@@ -47,30 +42,6 @@ public class Account
 
 	public void setAccNo(int accNo) {
 		this.accNo = accNo;
-	}
-
-	public String getAccess() {
-		return access;
-	}
-
-	public void setAccess(String access) {
-		this.access = access;
-	}
-
-	public String getPIN() {
-		return PIN;
-	}
-
-	public void setPIN(String pIN) {
-		PIN = pIN;
-	}
-
-	public int getBlz() {
-		return blz;
-	}
-
-	public void setBlz(int blz) {
-		this.blz = blz;
 	}
 
 	public long getBalance() {
