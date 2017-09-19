d6f0cea0cd7d9dc7ef03698b14485caaf95916d3
mholste <michael.holste@web.de>
2017-09-19 14:01:17 +0200

    Enität mit weiteren Atributen ergänzt

--- src/de/mho/finpim/persistence/model/Account.java
+++ src/de/mho/finpim/persistence/model/Account.java
@@ -23,10 +23,114 @@ public class Account
 	@JoinColumn(name="bId")
 	protected Bank bank;
 	
-	@Basic
-	protected int accNo;
+	@Basic(optional=false)
+	protected String accNo;
 	
+	@Basic
+	protected String type;
+	
+	@Basic
+	protected String iban;
+	
+	@Basic
+	protected String currency;
+	
+	@Basic
+	protected String limit;
+	
+	@Basic
+	protected String bic;
+	
+	@Basic
+	protected String blz;
+	
+	@Basic
+	protected String country;
+	
+	@Basic
+	protected String name;
+	
+	@Basic	
 	protected long balance;
+	
+	public Account() { }
+	
+	public Account(String accNo)
+	{
+		this.accNo = accNo;
+	}
+	
+	public Bank getBank() {
+		return bank;
+	}
+
+	public void setBank(Bank bank) {
+		this.bank = bank;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getIban() {
+		return iban;
+	}
+
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+	public String getLimit() {
+		return limit;
+	}
+
+	public void setLimit(String limit) {
+		this.limit = limit;
+	}
+
+	public String getBic() {
+		return bic;
+	}
+
+	public void setBic(String bic) {
+		this.bic = bic;
+	}
+
+	public String getBlz() {
+		return blz;
+	}
+
+	public void setBlz(String blz) {
+		this.blz = blz;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public Person getPerson() {
 		return person;
@@ -36,11 +140,11 @@ public class Account
 		this.person = person;
 	}
 
-	public int getAccNo() {
+	public String getAccNo() {
 		return accNo;
 	}
 
-	public void setAccNo(int accNo) {
+	public void setAccNo(String accNo) {
 		this.accNo = accNo;
 	}
 
